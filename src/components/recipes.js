078a759b2d872017b2adc1b0031e11e1f35565d8
Twahah Kaweesi <twahah.kaweesi@hotmail.com>
2025-06-20 10:54:36 +0300

    Initial commit

---
+++ src/components/recipes.js
@@ -0,0 +1,89 @@
+import { View, Text, Pressable, Image, StyleSheet, FlatList, TouchableOpacity } from "react-native";
+import React from "react";
+import {widthPercentageToDP as wp, heightPercentageToDP as hp,} from "react-native-responsive-screen";
+import { useNavigation } from "@react-navigation/native";
+
+export default function Recipe({ categories, foods }) {
+  const navigation = useNavigation();
+  const renderItem = ({ item, index }) => (
+  <ArticleCard item={item} index={index} navigation={navigation} />
+  );
+
+  return (
+    <View style={styles.container}>
+      <View testID="recipesDisplay">
+      
+        <Text style={styles.title}>Recipes</Text>
+          <FlatList
+            data={foods}
+            keyExtractor={(item) => item.idFood}
+            renderItem={renderItem}
+            numColumns={2}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: hp(2) }}
+          />
+      </View>
+    </View>
+  );
+}
+
+const ArticleCard = ({ item, index, navigation }) => {
+  return (
+    <View style={[styles.cardContainer, { paddingLeft: 20, paddingRight: 15}]} testID="articleDisplay">
+      <TouchableOpacity
+        onPress={() => navigation.navigate("RecipeDetail", { ...item  })}
+      >
+        <Image
+          source={{ uri: item.recipeImage }}
+          style={[styles.articleImage, { height: index % 3 === 0 ? hp(25) : hp(35) }]}
+        />
+          <Text style={styles.articleText}>{item.recipeName}</Text>
+          <Text style={styles.articleDescription}>
+            {item.recipeCategory} - {item.recipeOrigin}
+          </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: wp(4), // mx-4 equivalent
+    marginTop: hp(2),
+  },
+  title: {
+    fontSize: hp(3),
+    fontWeight: "600", // font-semibold
+    color: "#52525B", // text-neutral-600
+    marginBottom: hp(1.5),
+  },
+  loading: {
+    marginTop: hp(20),
+  },
+  cardContainer: {
+    justifyContent: "center",
+    marginBottom: hp(1.5),
+    flex: 1, // Allows cards to grow and fill space evenly
+  },
+  articleImage: {
+    width: "100%",
+    borderRadius: 35,
+    // backgroundColor: "rgba(0, 0, 0, 0.05)", // bg-black/5
+  },
+  articleText: {
+    fontSize: hp(3.5),
+    fontWeight: "600", // font-semibold
+    color: "#52525B", // text-neutral-600
+    marginLeft: wp(2),
+    marginTop: hp(0.5),
+  },
+  articleDescription: {
+    fontSize: hp(2.0),
+    color: "#6B7280", // gray-500
+    marginLeft: wp(2),
+    marginTop: hp(0.5),
+  },
+  row: {
+    justifyContent: "space-between", // Align columns evenly
+  },
+});
---
+++ src/screens/RecipesFormScreen.js
@@ -0,0 +1,114 @@
+import { View,Text,TextInput,TouchableOpacity,Image,StyleSheet,} from "react-native";
+import React, { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {widthPercentageToDP as wp,heightPercentageToDP as hp,} from "react-native-responsive-screen";
+
+export default function RecipesFormScreen({ route, navigation }) {
+  const { recipeToEdit, recipeIndex, onrecipeEdited } = route.params || {};
+  const [title, setTitle] = useState(recipeToEdit ? recipeToEdit.title : "");
+  const [image, setImage] = useState(recipeToEdit ? recipeToEdit.image : "");
+  const [description, setDescription] = useState(
+    recipeToEdit ? recipeToEdit.description : ""
+  );
+
+  const saverecipe = async () => {
+    const newrecipe = { title, image, description };
+    try {
+      const existingRecipes = await AsyncStorage.getItem("customRecipes");
+      const recipes = existingRecipes ? JSON.parse(existingRecipes) : [];
+
+      // If editing a recipe, update it; otherwise, add a new one
+      if (recipeToEdit !== undefined) {
+        recipes[recipeIndex] = newrecipe;
+        await AsyncStorage.setItem("customRecipes", JSON.stringify(recipes));
+        if (onrecipeEdited) onrecipeEdited(); // Notify the edit
+      } else {
+        recipes.push(newrecipe); // Add new recipe
+        await AsyncStorage.setItem("customRecipes", JSON.stringify(recipes));
+      }
+
+      navigation.goBack(); // Return to the previous screen
+    } catch (error) {
+      console.error("Error saving the recipe:", error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+        <Text style={styles.backButtonText}>{"Back"}</Text>
+      </TouchableOpacity>
+      <Text style={{ fontSize: wp(2), fontWeight: "bold", marginBottom: hp(2) }}>
+        {recipeToEdit ? "Edit Recipe" : "Add New Recipe"}
+      </Text>
+      <TextInput
+        placeholder="Title"
+        value={title}
+        onChangeText={setTitle}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Image URL"
+        value={image}
+        onChangeText={setImage}
+        style={styles.input}
+      />
+      {image ? (
+        <Image source={{ uri: image }} style={styles.image} />
+      ) : (
+        <Text style={styles.imagePlaceholder}>Upload Image URL</Text>
+      )}
+      <TextInput
+        placeholder="Description"
+        value={description}
+        onChangeText={setDescription}
+        multiline={true}
+        numberOfLines={4}
+        style={[styles.input, { height: hp(20), textAlignVertical: "top" }]}
+      />
+      <TouchableOpacity onPress={saverecipe} style={styles.saveButton}>
+        <Text style={styles.saveButtonText}>Save recipe</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: wp(4),
+  },
+  input: {
+    marginTop: hp(4),
+    borderWidth: 1,
+    borderColor: "#ddd",
+    padding: wp(.5),
+    marginVertical: hp(1),
+  },
+  image: {
+    width: 300,
+    height:200,
+    margin: wp(2),
+  },
+  imagePlaceholder: {
+    height: hp(20),
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: hp(1),
+    borderWidth: 1,
+    borderColor: "#ddd",
+    textAlign: "center",
+    padding: wp(2),
+  },
+  saveButton: {
+    backgroundColor: "#4F75FF",
+    padding: wp(.5),
+    alignItems: "center",
+    borderRadius: 5,
+    marginTop: hp(2),
+  },
+  saveButtonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
